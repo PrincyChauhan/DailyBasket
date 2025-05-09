2ea8b6b1b7e08f6f9d436a780caba5632e6d9190
PrincyChauhan <chauhanprincee7@gmail.com>
2025-05-09 11:15:33 +0530

    App context created

---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,16 @@
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export const AppContext = createContext();
+export const AppContextProvider = ({ Children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [isSeller, setIsSeller] = useState(false);
+
+  const value = { navigate, user, setUser, isSeller, setIsSeller };
+  return <AppContext.Provider value={value}>{Children}</AppContext.Provider>;
+};
+
+export const useAppContext = () => {
+  return useContext(AppContext);
+};
