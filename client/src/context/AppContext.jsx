118074575e84a7e0aed54b5bf4c7e35b342b63f4
PrincyChauhan <chauhanprincee7@gmail.com>
2025-05-10 11:25:33 +0530

    All product

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -13,6 +13,7 @@ export const AppContextProvider = ({ children }) => {
   const [showUserLogin, setShowUserLogin] = useState(false);
   const [products, setProducts] = useState([]);
   const [cartItems, setCartItems] = useState({});
+  const [searchQuery, setSearchQuery] = useState({});
 
   const fetchProducts = async () => {
     setProducts(dummyProducts);
@@ -75,6 +76,8 @@ export const AppContextProvider = ({ children }) => {
     updateCartQuantity,
     removeFromCart,
     cartItems,
+    searchQuery,
+    setSearchQuery,
   };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
