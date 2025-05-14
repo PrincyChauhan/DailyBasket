edeb422607d5a66dbd4b083a769e1ae449a8583c
PrincyChauhan <chauhanprincee7@gmail.com>
2025-05-14 15:29:56 +0530

    Orders API

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ import productRouter from "./routes/productRoute.js";
 import cartRouter from "./routes/cartRoute.js";
 import addressRouter from "./routes/addressRoute.js";
 import connectCloudinary from "./config/cloudinary.js";
+import orderRouter from "./routes/orderRoute.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -29,6 +30,7 @@ app.use("/api/seller", sellerRouter);
 app.use("/api/product", productRouter);
 app.use("/api/cart", cartRouter);
 app.use("/api/address", addressRouter);
+app.use("/api/order", orderRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port} `);
