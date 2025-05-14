d951328c7a35c2bd036412c2d44832849067fc9b
PrincyChauhan <chauhanprincee7@gmail.com>
2025-05-14 17:08:16 +0530

    Product List API

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,6 +1,5 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { dummyProducts } from "../assets/assets";
 import toast from "react-hot-toast";
 import axios from "axios";
 
@@ -34,7 +33,16 @@ export const AppContextProvider = ({ children }) => {
   };
 
   const fetchProducts = async () => {
-    setProducts(dummyProducts);
+    try {
+      const { data } = await axios.get("/api/product/list");
+      if (data.success) {
+        setProducts(data.products);
+      } else {
+        toast.error(data.message);
+      }
+    } catch (error) {
+      toast.error(error.message);
+    }
   };
 
   //   add product to cart
@@ -117,6 +125,7 @@ export const AppContextProvider = ({ children }) => {
     setSearchQuery,
     getCartAmount,
     getCartCount,
+    fetchProducts,
     axios,
   };
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
